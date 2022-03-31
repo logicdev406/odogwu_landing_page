eecbdae0a34d347ed9b7a954d8cd5153bce4b8b2
Sunday Ochuko <sundayochuko101@gmail.com>
2022-03-31 23:23:53 +0100

    made the helth benefit section of the home page fully respnosive

--- src/pages/Home/components/HelthBenefits.js
+++ src/pages/Home/components/HelthBenefits.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const HelthBenefits = () => {
-  return <div className=" h-[400px] w-full bg-black ">HelthBenefits</div>;
+  return (
+    <div className=" flex flex-col items-center justify-center h-auto md:h-auto lg:h-[400px] w-full bg-black font-poppins py-10 md:py-20 lg:py-0 ">
+      <h1 className=" text-xl md:text-4xl lg:text-4xl text-lemonGreen font-semibold ">
+        Health benefits
+      </h1>
+      <p className=" text-sm md:text-lg lg:text-lg text-white font-light mt-[20px] text-center px-5 md:px-20 lg:px-0 ">
+        Odogwu bitters aids in boosting the immune system helps in the relief of
+        stress <br className=" hidden md:hidden lg:inline-block " /> and
+        regulation of blood sugar level and promotion of weight loss..
+      </p>
+    </div>
+  );
 };
 
 export default HelthBenefits;
